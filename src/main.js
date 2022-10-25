87b2c482ff6f718f9bf74df416d02ee8ef3764e9
Muhammad Taha <tahasiddiqi89@gmail.com>
2022-10-25 14:19:12 +0500

    Added middleware code

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const api = express();
 
+//using express middleware and static method
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
     console.log('API is up and running');
 });
 
-api.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello World');
-});
+
